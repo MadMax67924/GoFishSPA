6b1ef97821c9a7817e4ba45ec696006f5d726907
MaximilianoGarin <maxigarin06@gmail.com>
2025-11-18 17:02:10 -0300

    cambios para vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,5 +10,12 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+const path = require('path');
 
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 export default nextConfig
